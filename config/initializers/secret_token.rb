601cf262fbf9f6e10e72c943ef4e086e5eaba025
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2011-05-21 16:35:47 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovepulse::Application.config.secret_token = 'adcd0810937250e7ae677412dfe607465037b6a95384789c16b8e4462ed25a01f26457822289a3590b88c0bf5e7b6d5ad00a772ef4038d5c20fb974e3b5c0bb1'
